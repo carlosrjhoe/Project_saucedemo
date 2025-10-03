32874eb18c1bf7f2e27f3c9fa8eb430bf5cf63e3
carlosrjhoe <carlosrjhoe@gmail.com>
2025-10-03 05:23:55 -0300

    TEST: test_login_with_valid_username PASSED

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,7 +2,17 @@ from pages.login_page import LoginPage
 
 def test_valid_login(driver):
     login_page = LoginPage(driver)
-    login_page.open_login_page()
-    result = login_page.title_login_page()
+    login_page.open_swag_page()
+    result = login_page.title_access_page()
     expected = 'Swag Labs'
+    assert result == expected
+
+def test_login_with_valid_username(driver):
+    login_page = LoginPage(driver)
+    login_page.open_swag_page()
+    login_page.set_username()
+    login_page.set_password()
+    login_page.click_buton_login()
+    result = login_page.title_product_page()
+    expected = 'Products'
     assert result == expected
